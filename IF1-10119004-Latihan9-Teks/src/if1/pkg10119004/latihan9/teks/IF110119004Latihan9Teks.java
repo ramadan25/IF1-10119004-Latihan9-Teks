b7a862f0a2e4cc40ef4defe2ce526d74c31e365f
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-20 11:26:39 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan9-Teks/src/if1/pkg10119004/latihan9/teks/IF110119004Latihan9Teks.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan9.teks;
+
+/**
+ *
+ * @author ENDOG
+ * NAMA     : Muhamad Ramadan
+ * KELAS    : IF 1
+ * NIM      : 10119004
+ * Deskripsi Program : Program ini berisi untuk menampilkan
+ * value ke layar
+ */
+public class IF110119004Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
